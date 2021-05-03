89ad5ae46fec58f31f5c92d3bd9ee1b220b27b4e
Carlos <carlosrh98@gmail.com>
2021-05-03 16:47:49 +0200

    getUrl

--- rewards/src/App.js
+++ rewards/src/App.js
@@ -4,12 +4,17 @@ import RewardsSuccess from './components/RewardSuccess.js';
 import './App.css';
 
 class App extends Component {
+  componentDidMount = async () => {
+    this.getUrlInfo();
+  }
   constructor(props){
     super(props);
     this.state = {
       isSubmitted: false,
       isSuccess: true,
       tipAmount: 0,
+      from: null,
+      to: null,
     }
 
     this.setSubmission = this.setSubmission.bind(this);
@@ -24,11 +29,20 @@ class App extends Component {
     this.setState({tipAmount: amount})
   }
 
+  getUrlInfo(){
+    let url_string = window.location.href
+    let url = new URL(url_string);
+    let _from = url.searchParams.get("from");
+    let _to = url.searchParams.get("to");
+
+    this.setState({from: _from, to: _to})
+  }
+
   render(){
     return(
       <div>
       {!this.state.isSubmitted ? (
-        <Rewards setSubmission={this.setSubmission} setTipAmount={this.setTipAmount}/>
+        <Rewards setSubmission={this.setSubmission} setTipAmount={this.setTipAmount} from={this.state.from} to={this.state.to}/>
       ):(
         <RewardsSuccess isSuccess={this.state.isSuccess} setSubmission={this.setSubmission}/>
       )}
